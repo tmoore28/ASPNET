f6886c82345e229ab946245160c4e710fef679be
tylor moore <mooretylor28@gmail.com>
2022-04-27 14:58:36 -0400

    Third Exercise

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -8,6 +8,7 @@ using Testing.Models;
         {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
 
         }
     }
