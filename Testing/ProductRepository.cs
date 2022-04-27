4dd5142ecfba24f0ffb894264f8a5468e762aebe
tylor moore <mooretylor28@gmail.com>
2022-04-27 14:43:02 -0400

    Exercise 1

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
